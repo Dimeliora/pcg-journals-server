6007d12fa301637791d1b0b18bd4a4e993f3e293
dimeliora <dimeliora710@gmail.com>
2021-11-10 16:57:10 +0900

    computer schema pre-save hooks, defaults

--- src/modules/computers/schemas/computer.schema.ts
+++ src/modules/computers/schemas/computer.schema.ts
@@ -18,77 +18,89 @@ export class Computer extends Base {
   @Prop({ type: String, required: true })
   pcPurpose: string;
 
-  @Prop()
+  @Prop({ type: String, default: 'н/д' })
   formFactor: string;
 
-  @Prop()
+  @Prop({ type: String, default: 'н/д' })
   motherboard: string;
 
-  @Prop()
+  @Prop({ type: String, default: 'н/д' })
   typeOfSocket: string;
 
-  @Prop()
+  @Prop({ type: String, default: 'н/д' })
   numOfSockets: string;
 
-  @Prop()
+  @Prop({ type: String, default: 'н/д' })
   maxRamSize: string;
 
-  @Prop()
+  @Prop({ type: String, default: 'н/д' })
   sataType: string;
 
-  @Prop()
+  @Prop({ type: String, default: 'н/д' })
   numOfSataPorts: string;
 
-  @Prop()
+  @Prop({ type: String, default: 'н/д' })
   numOfPsu: string;
 
-  @Prop()
+  @Prop({ type: String, default: 'н/д' })
   psuPower: string;
 
-  @Prop()
+  @Prop({ type: String, default: 'н/д' })
   os: string;
 
-  @Prop()
+  @Prop({ type: String, default: 'н/д' })
   numOfCpu: string;
 
-  @Prop()
+  @Prop({ type: String, default: 'н/д' })
   cpuModel: string;
 
-  @Prop()
+  @Prop({ type: String, default: 'н/д' })
   numOfCores: string;
 
-  @Prop()
+  @Prop({ type: String, default: 'н/д' })
   cpuFrequency: string;
 
-  @Prop()
+  @Prop({ type: String, default: 'н/д' })
   cpuL1Cache: string;
 
-  @Prop()
+  @Prop({ type: String, default: 'н/д' })
   cpuL2Cache: string;
 
-  @Prop()
+  @Prop({ type: String, default: 'н/д' })
   cpuL3Cache: string;
 
-  @Prop()
+  @Prop({ type: String, default: 'н/д' })
   ramType: string;
 
-  @Prop()
+  @Prop({ type: String, default: 'н/д' })
   ramTotalModules: string;
+
+  @Prop({ type: String, default: 'н/д' })
+  ramTotalSize: string;
 
   @Prop([RamModule])
   ramModules: RamModule[];
 
-  @Prop()
+  @Prop({ type: String, default: 'н/д' })
   raid: string;
 
-  @Prop()
+  @Prop({ type: String, default: 'н/д' })
   raidMode: string;
 
-  @Prop()
+  @Prop({ type: String, default: 'н/д' })
   totalDiskSpace: string;
 
   @Prop([HDD])
   disks: HDD[];
+
+  @Prop({ type: String, default: 'н/д' })
+  cpuUpgrade: string;
+
+  @Prop({ type: String, default: 'н/д' })
+  ramUpgrade: string;
+
+  @Prop({ type: String, default: 'н/д' })
+  hddUpgrade: string;
 
   @Prop([PCBackup])
   pcBackups: PCBackup[];
@@ -101,3 +113,64 @@ export class Computer extends Base {
 }
 
 export const ComputerSchema = SchemaFactory.createForClass(Computer);
+
+ComputerSchema.pre<Computer>('save', function (next) {
+  const ramTotalSize = this.ramModules.reduce(
+    (sum, item) => sum + Number.parseFloat(item.ramModuleSize),
+    0,
+  );
+
+  if (ramTotalSize > 0) {
+    this.ramTotalSize = ramTotalSize.toFixed(1);
+  }
+
+  next();
+});
+
+ComputerSchema.pre<Computer>('save', function (next) {
+  const numOfSockets = Number.parseInt(this.numOfSockets, 10);
+  const numOfCpu = Number.parseInt(this.numOfCpu, 10);
+
+  if (!Number.isNaN(numOfSockets) && !Number.isNaN(numOfCpu)) {
+    this.cpuUpgrade = this.numOfSockets > this.numOfCpu ? 'Да' : 'Нет';
+  }
+
+  next();
+});
+
+ComputerSchema.pre<Computer>('save', function (next) {
+  const ramTotalModules = Number.parseInt(this.ramTotalModules, 10);
+  const maxRamSize = Number.parseInt(this.maxRamSize, 10);
+  const ramTotalSize = Number.parseInt(this.ramTotalSize, 10);
+
+  if (
+    Number.isNaN(ramTotalModules) ||
+    Number.isNaN(maxRamSize) ||
+    Number.isNaN(ramTotalSize)
+  ) {
+    return next();
+  }
+
+  const additionalModules = ramTotalModules - this.ramModules.length;
+  const availableSpace = maxRamSize - ramTotalSize;
+
+  if (additionalModules > 0 && availableSpace > 0) {
+    this.ramUpgrade = `Модулей: ${additionalModules}, суммарно на: ${availableSpace} Гб`;
+  } else {
+    this.ramUpgrade = 'Нет';
+  }
+
+  next();
+});
+
+ComputerSchema.pre<Computer>('save', function (next) {
+  const numOfSataPorts = Number.parseInt(this.numOfSataPorts, 10);
+
+  if (!Number.isNaN(numOfSataPorts)) {
+    const additionalDisks = numOfSataPorts - this.disks.length;
+    this.hddUpgrade =
+      additionalDisks > 0 ? `Дисков: ${additionalDisks}` : 'Нет';
+  }
+
+  next();
+});
